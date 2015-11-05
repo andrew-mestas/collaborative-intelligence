e823f731cc3ae4a1c70aeca72cf7fb1dd6ec9aed
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-05 09:25:32 -0800

    heroku test with order of answer by sorting questions

--- test.js
+++ test.js
@@ -358,9 +358,9 @@ db.user.find({where: {
 			include: [db.question],
 		}).then(function(questionItem){
 		db.question.findAll({
-			include: [db.answer]
-
-			}).then(function(answerItem){
+			include: [db.answer],
+			order: 'categoryId'
+		}).then(function(answerItem){
 
 			questionItem.forEach(function(qt, counter){
 				count= counter;
