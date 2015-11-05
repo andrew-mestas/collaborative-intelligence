7b955d880b23f333fdbe1679ad841d0cf07bd2c6
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-05 09:48:11 -0800

    heroku test order category by id question by id and answer by category id

--- test.js
+++ test.js
@@ -359,7 +359,7 @@ db.user.find({where: {
 		}).then(function(categoryItem){
 		db.question.findAll({
 			include: [db.answer],
-			order: [[db.answer, "questionId"]]
+			order: [[db.answer, "categoryId"]]
 		}).then(function(questionItem){
 
 			categoryItem.forEach(function(qt, counter){
