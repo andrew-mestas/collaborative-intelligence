6257540507b4a10a0702c17cb4269bc7cfc5c0df
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-04 17:30:01 -0800

    more heroku stuff

---
+++ migrations/20151104172912-addadmintouser.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+      return queryInterface.addColumn('users', 'admin', {
+       type: Sequelize.BOOLEAN
+     });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+      return queryInterface.addColumn('users', 'admin');
+  }
+};
